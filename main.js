dfcd3af956ee1999256c1364331ae70c3ed9ee66
Darko <bazalac.darko@outlook.com>
2022-11-03 12:40:17 +0100

    Added Handling errors functionality

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
-const searchInput = document.querySelector("input");
 const searchBtn = document.querySelector(".submit-btn");
 const form = document.querySelector("form");
 const mainContainer = document.getElementById("main-container");
+const error = document.querySelector(".error-msg");
 let city = "Kraljevo";
 getData(city);
 
-searchBtn.addEventListener("click", e => {
-  e.preventDefault();
-  fetchWeather();
-});
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  fetchWeather();
-});
+searchBtn.addEventListener("click", fetchWeather);
+form.addEventListener("submit", fetchWeather);
 
 async function getData(city) {
-  const resp = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4045f7b026733291f6fe104d28c865c9&units=metric`,
-    { mode: "cors" },
-  );
-  const data = await resp.json();
-  const newData = processData(data);
-  mainContainer.style.backgroundImage = `url("images/${newData.mainWeather}.jpg")`;
-  console.log(newData, data);
-  // displayData(newData);
-  form.reset();
+  try {
+    const resp = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4045f7b026733291f6fe104d28c865c9&units=metric`,
+      { mode: "cors" },
+    );
+    error.style.display = "none";
+    const data = await resp.json();
+    const newData = processData(data);
+    console.log(newData, data);
+    displayData(newData);
+    form.reset();
+  } catch (err) {
+    error.style.display = "block";
+    if (error.classList.contains("fade-in")) {
+      error.style.display = "none";
+      error.classList.remove("fade-in2");
+      error.offsetWidth;
+      error.classList.add("fade-in");
+      error.style.display = "block";
+    } else {
+      error.classList.add("fade-in");
+    }
+  }
 }
 
-function fetchWeather() {
+function fetchWeather(e) {
+  e.preventDefault();
   const input = document.querySelector('input[type="text"]');
   const userLocation = input.value;
   getData(userLocation);
@@ -43,4 +50,8 @@ function processData(data) {
     mainWeather: data.weather[0].main,
   };
   return myData;
+}
+
+function displayData(data) {
+  mainContainer.style.backgroundImage = `url("images/${data.mainWeather}.jpg")`;
 }
