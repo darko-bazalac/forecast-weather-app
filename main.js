5ed116a52eb910ea237a3671b226ee669e8cf789
Darko <bazalac.darko@outlook.com>
2022-11-04 18:45:33 +0100

    Added button for temp switch

--- main.js
+++ main.js
@@ -2,17 +2,22 @@ const searchBtn = document.querySelector(".submit-btn");
 const form = document.querySelector("form");
 const mainContainer = document.getElementById("main-container");
 const errorMsg = document.querySelector(".error-msg");
+let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+const toggleSwitch = document.querySelector("[switch]");
 let city = "Kraljevo";
-getData(city);
+getData(city, "metric");
 
 searchBtn.addEventListener("click", fetchWeather);
 form.addEventListener("submit", fetchWeather);
+// celsiusBtn.addEventListener("click", () => {
+//   getData(city, "imperial");
+// });
 
 // Fetching API
-async function getData(city) {
+async function getData(city, units) {
   try {
     const resp = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4045f7b026733291f6fe104d28c865c9&units=metric`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4045f7b026733291f6fe104d28c865c9&units=${units}`,
       { mode: "cors" },
     );
     const data = await resp.json();
@@ -40,7 +45,8 @@ function fetchWeather(e) {
   e.preventDefault();
   const input = document.querySelector('input[type="text"]');
   const userLocation = input.value;
-  getData(userLocation);
+  city = userLocation;
+  getData(userLocation, "metric");
 }
 
 // Processing collected data
@@ -54,17 +60,22 @@ function processData(data) {
     wind: Math.round(data.wind.speed * (18 / 5)),
     humidity: data.main.humidity,
     mainWeather: data.weather[0].main,
+    timezone: data.timezone,
   };
   return myData;
 }
-
 // Displaying data on page
 function displayData(data) {
   mainContainer.style.backgroundImage = `url("images/${data.mainWeather}.jpg")`;
 
-  const cityName = (document.querySelector(
-    ".location",
-  ).innerHTML = `${data.city}, ${data.country}`);
+  const dateAndTime = data.timezone;
+
+  const datePreview = (document.querySelector(".date").innerHTML =
+    getLocalTime(dateAndTime));
+
+  const cityName = (document.querySelector(".location").innerHTML = `${
+    data.city
+  }, ${regionNames.of(data.country)}`);
 
   const temp = (document.querySelector(".temp").innerHTML = `${data.temp}`);
 
@@ -75,10 +86,23 @@ function displayData(data) {
   const tempFeeling = (document.querySelector(
     ".feels-like",
   ).innerHTML = `FEELS LIKE: ${data.feelsLike}`);
+
   const humidity = (document.querySelector(
     ".humidity",
   ).innerHTML = `HUMIDITY: ${data.humidity}`);
+
   const wind = (document.querySelector(
     ".wind",
   ).innerHTML = `WIND: ${data.wind} km/h`);
+}
+
+/*Get local time function */
+function getLocalTime(data) {
+  let date = new Date();
+  let time = date.getTime();
+  let localOffset = date.getTimezoneOffset() * 60000;
+  let utc = time + localOffset;
+  let localTime = utc + 1000 * data;
+  let localTimeDate = new Date(localTime);
+  return localTimeDate.toLocaleString();
 }
